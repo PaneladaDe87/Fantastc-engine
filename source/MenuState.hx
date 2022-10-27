61cfa39a9f453dd25b07f05a70a0668142ccbcdc
PaneladaDe87 <102541192+PaneladaDe87@users.noreply.github.com>
2022-10-27 12:21:29 -0300

    Update MenuState.hx

--- source/MenuState.hx
+++ source/MenuState.hx
@@ -1,12 +1,16 @@
 package;
 
-import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.FlxState;
+import flixel.FlxColor;
 
 class MenuState {
+     var Text:FlxText;
      override public function Create():void {
           super.Create();
+          Text = new FlxText(0, 0, FlxG.width, "hello, user", 64);
+          Text.setFormat(null, 0, FlxColor.WHITE, FlxTextAlign.CENTER);
+          add(Text);
      }
      override public function Update(elapsed:float):void {
           super.update(elapsed);
