9ae8094727808fc2466959bb73df1a3a430b3d09
PaneladaDe87 <102541192+PaneladaDe87@users.noreply.github.com>
2022-10-27 12:02:29 -0300

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -7,6 +7,6 @@ import openfl.display.Sprite;
 class Main extends Sprite {
      public function new() {
          super();
-         addChild(new FlxGame(0, 0, menuState));
+         addChild(new FlxGame(0, 0, MenuState));
      }
 }
