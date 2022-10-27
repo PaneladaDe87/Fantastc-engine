1194c498fe0e5d0d6d567433e2ee03f96341c6ff
PaneladaDe87 <102541192+PaneladaDe87@users.noreply.github.com>
2022-10-27 12:09:39 -0300

    Update MenuState.hx

--- source/MenuState.hx
+++ source/MenuState.hx
@@ -1,1 +1,14 @@
+package;
 
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.FlxState;
+
+class MenuState {
+     override public function Create():void {
+          super.Create();
+     }
+     override public function Update(elapsed:float):void {
+          super.update(elapsed);
+     }
+}
